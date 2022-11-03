9c26b86df889f815236180f1eebe9a19532cf200
Vickky143 <109405599+Vickky143@users.noreply.github.com>
2022-11-03 12:00:32 +0530

    n=int(input()) m=int(n**0.5) for i in range(1,m+1):     x=i*(i+1)     if x==n:         print("YES")         break else:     print("NO")

--- Pronic_Number_or_Heteromecic_Number.py
+++ Pronic_Number_or_Heteromecic_Number.py
@@ -1,7 +1,9 @@
 n=int(input())
-for i in range(1,n+1):
-    if i*(i+1)==n:
-        print('YES')
+m=int(n**0.5)
+for i in range(1,m+1):
+    x=i*(i+1)
+    if x==n:
+        print("YES")
         break
 else:
-    print('NO')
+    print("NO")
